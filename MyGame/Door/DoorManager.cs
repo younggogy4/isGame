5abf6933f9367db2399d4fef6b495ffeb7f3c485
Якшибаев Данил Димович <150374253+younggogy4@users.noreply.github.com>
2024-04-29 17:52:57 +0300

    Add files via upload

---
+++ MyGame/Door/DoorManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace MyGame;
+
+public class DoorManager
+{
+    public List<DoorData> Doors = new();
+    private Texture2D texture;
+    private List<Vector2> positionsDoor = new();
+    public bool CanEnter = false;
+
+    public void Init(GameData gameData, RoomManager roomManager)
+    {
+        texture = Globals.Content.Load<Texture2D>("blockDoor");
+        positionsDoor.Add(new Vector2(0, gameData.Height / 2 - texture.Height));
+        positionsDoor.Add(new Vector2(gameData.Width - 150, gameData.Height / 2 - texture.Height));
+        //positionsDoor.Add(new Vector2(gameData.Width / 2 - texture.Width, gameData.Height - 150));
+        //positionsDoor.Add(new Vector2(gameData.Width / 2 - texture.Width, 0));
+        Fill(gameData, roomManager);
+    }
+
+    public void Fill(GameData gameData, RoomManager roomManager)
+    {
+        /*var door = new DoorData();
+        door.Position = positionsDoor[0];
+        door.Bounds = door.Bounds = new Rectangle((int)door.Position.X, (int)door.Position.Y, 200, 200);
+        Doors.Add(door);*/
+        var door1 = new DoorData();
+        door1.Position = positionsDoor[1];
+        door1.Bounds = door1.Bounds = new Rectangle((int)door1.Position.X, (int)door1.Position.Y, 400, 400);
+         Doors.Add(door1);
+        /*for (var i = 0; i < gameData.DoorData.countDoor; i++)
+        {
+            var random = new Random().Next(0, 4);
+            while (Doors.Any(x => x.Position == positionsDoor[random])) random = new Random().Next(0, 3);
+            var door = new DoorData();
+            door.Position = positionsDoor[random];
+            door.Bounds = new Rectangle((int)door.Position.X, (int)door.Position.Y, 700, 700);
+            Doors.Add(door);
+        }*/
+    }
+
+    public void Update(GameData gameData)
+    {
+        if (gameData.EnemyManager.Enemies.Count <= 0)
+        {
+            texture = Globals.Content.Load<Texture2D>("door");
+            CanEnter = true;
+        }
+    }
+
+    public void Draw(GameData gameData)
+    {
+        foreach (var door in Doors)
+        {
+            Globals.SpriteBatch.Draw(texture,
+                new Rectangle((int)door.Position.X, (int)door.Position.Y, gameData.Width / 14, gameData.Height / 8),
+                Color.White);
+        }
+    }
+}
---
+++ MyGame/Game/GameUpdate.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+using Microsoft.Xna.Framework;
+
+namespace MyGame;
+
+public class GameUpdate
+{
+    private void UpdateHearts(GameData gameData)
+    {
+        if (gameData.CountUpdateHeroHelths >= 1) return;
+        gameData.CountUpdateHeroHelths++;
+        gameData.HeartManager.FillHearts(gameData);
+    }
+    
+    private void UpdateTear(GameData gameData)
+    {
+        if (gameData.HeroUpdate.Healths <= 0) return;
+        if (InputManager.DirectionShoot != Button.None)
+        {
+            var direction = Vector2.Zero;
+
+            switch (InputManager.DirectionShoot)
+            {
+                case Button.Right:
+                    direction = new Vector2(1, 0);
+                    break;
+                case Button.Left:
+                    direction = new Vector2(-1, 0);
+                    break;
+                case Button.Down:
+                    direction = new Vector2(0, 1);
+                    break;
+                case Button.Up:
+                    direction = new Vector2(0, -1);
+                    break;
+            }
+
+            var distance = direction * gameData.Width;
+            gameData.TearData.Rotation = (float)Math.Atan2(distance.Y, distance.X);
+            gameData.TearsManager.Shoot(gameData);
+        }
+
+       gameData.TearsManager.Tears
+            .Where(tear => gameData.EnemyManager.Enemies.Any(enemy => enemy.Bounds.Contains(tear.Position)))
+            .Select(tear => gameData.TearsManager.Tears.Remove(tear))
+            .ToList();
+       gameData.TearsManager.Update();
+    }
+    
+    public void Update(GameData gameData)
+    {
+        InputManager.Update();
+        gameData.EnemyManager.Update(gameData);
+        UpdateTear(gameData);
+        UpdateHearts(gameData);
+        gameData.HeroUpdate.Update(gameData);
+        gameData.DropHeartUpdate.Update(gameData);
+        gameData.BombManager.Update(gameData);
+        gameData.DoorManager.Update(gameData);
+    }
+}
